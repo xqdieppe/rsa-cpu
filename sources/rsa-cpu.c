3877757bdbb3a1be10d0e772596785edd2e812a4
qdieppe <qdieppe@debian-BULLSEYE-live-builder-AMD64>
2023-10-03 20:57:52 +0200

    udiv2 ok

--- sources/rsa-cpu.c
+++ sources/rsa-cpu.c
@@ -3,10 +3,10 @@
 int main(int argc, char **argv) {
 	srand(time(0));
 
-	#define USIZE 256 
+	#define USIZE 512
 	u(unit0, USIZE); u(unit1, USIZE); u(uresult, USIZE);
 	//memset(unit0, 0xee, 512 / 8); memset(unit1, 0xff, 256 / 8);
-	urand(unit0, 64); urand(unit1, 32);
+	urand(unit0, 512); urand(unit1, 384);
 	udiv2(unit0, unit1, uresult, USIZE);
 	udump(unit0, USIZE); udump(unit1, USIZE); udump(uresult, USIZE);
 	
--- sources/udiv2.c
+++ sources/udiv2.c
@@ -4,12 +4,11 @@ void udiv2(uint32_t *unit0, uint32_t *unit1, uint32_t *result, size_t bits) {
 	u(u0, bits); uassign(u0, unit0, bits); u(block, bits);
 
 	for (size_t i = 0; i < bits; i++) {
-		if (usup(unit1, block, bits)) {
-			u(tmpblock, bits); ulshift(block, tmpblock, 1, bits); uassign(block, tmpblock, bits);
-			block[0] |= (u0[bits / 32 - 1]) >> 31;
+		u(tmpblock, bits); ulshift(block, tmpblock, 1, bits); uassign(block, tmpblock, bits);
+		block[0] |= (u0[bits / 32 - 1]) >> 31;
+		u(tmpu0, bits); ulshift(u0, tmpu0, 1, bits); uassign(u0, tmpu0, bits);
+		if (usup(unit1, block, bits)) {	
 			u(tmpresult, bits); ulshift(result, tmpresult, 1, bits); uassign(result, tmpresult, bits);
-			
-			u(tmpu0, bits); ulshift(u0, tmpu0, 1, bits); uassign(u0, tmpu0, bits);
 		} else {
 			u(tmpblock, bits); usub(block, unit1, tmpblock, bits); uassign(block, tmpblock, bits);	
 			u(tmpresult, bits); ulshift(result, tmpresult, 1, bits); uassign(result, tmpresult, bits);
