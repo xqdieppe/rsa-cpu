dbc5e3408dac2797da350eafb46bd78ddb882bb3
qdieppe <qdieppe@debian-BULLSEYE-live-builder-AMD64>
2023-10-07 10:21:11 +0200

    better args

--- sources/rsa-cpu.c
+++ sources/rsa-cpu.c
@@ -1,15 +1,52 @@
 #include "rsa-cpu.h"
 
 void help(char **argv) {
-	printf("usage: ./%s <mode> <...paramters>\n", argv[0]);
+	printf("usage: %s --mode <mode> <...paramters>\n", argv[0]);
+	printf("%s --mode keygen --size <keysize> --directory <dirname>\n", argv[0]);
+	printf("%s --mode encrypt --pubkey <pubkey> --message <message> --file <file>\n", argv[0]);
+	printf("%s --mode decrypt --privkey <privkey> --file <file>\n", argv[0]);
+}
+
+char *get_parameter(int argc, char **argv, char *parameter) {
+	for (int i = 0; i < argc; i++) {
+		if (!strcmp(argv[i], parameter) && (i + 1) < argc)
+			return (argv[i + 1]);
+	}
+	return (NULL);
+}
+
+int keygen_args(int argc, char **argv) {
+	char *directory = get_parameter(argc, argv, "--directory");
+	char *size = get_parameter(argc, argv, "--size");
+	int s = atoi(size);
+
+	keygen(directory, s);
+	return (1);
+}
+
+int encrypt_args(int argc, char **argv) {
+	char *pubkey = get_parameter(argc, argv, "--pubkey");
+	char *message = get_parameter(argc, argv, "--message");
+	char *file = get_parameter(argc, argv, "--file");
+
+	encrypt(message, pubkey, file);
+	return (1);
+}
+
+int decrypt_args(int argc, char **argv) {
+	char *privkey = get_parameter(argc, argv, "--privkey");
+	char *file = get_parameter(argc, argv, "--file");
+
+	decrypt(privkey, file);
+	return (1);
 }
 
 int main(int argc, char **argv) {
 	srand(time(0));
-
-	if (argc < 1) { help(argv); return (1); }
-	else if (!strcmp(argv[1], "keygen")) keygen(argv[2], atoi(argv[3]));
-	else if (!strcmp(argv[1], "encrypt")) encrypt(argv[2], argv[3], argv[4]);
-	else if (!strcmp(argv[1], "decrypt")) decrypt(argv[2], argv[3]);
+	char *mode = get_parameter(argc, argv, "--mode");
+	if (mode == NULL) { help(argv); return (1); }
+	else if (!strcmp(mode, "keygen")) { return (keygen_args(argc, argv)); }
+	else if (!strcmp(mode, "encrypt")) { return (encrypt_args(argc, argv)); } 
+	else if (!strcmp(mode, "decrypt")) { return (decrypt_args(argc, argv)); }
 	else { help(argv); return (1); }
 }
